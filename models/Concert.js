4ca5df0b0c9e2bbdaac5f767d54e8cf5c91cd37a
elaitala <elaitala@gmail.com>
2020-01-31 10:42:50 -0800

    Adds CONCERT model

---
+++ models/Concert.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+console.log('CONCERT working...');
+
+const ConcertSchema = new Schema ({
+  date: String,
+  venue: String,
+  setlist: String,
+  weather: String,
+  friends: String
+  // 
+
+});
+
+const Concert = mongoose.model('Concert', ConcertSchema);
+
+module.exports = Trainer;
